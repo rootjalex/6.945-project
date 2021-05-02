e7f86a3d80983be3113858ec6fa212d5ed11821f
Alexander Root <aroot222@mit.edu>
2021-05-02 16:25:33 -0400

    add pair-type & example

--- src/temp.scm
+++ src/temp.scm
@@ -98,6 +98,105 @@
 |#
 ;Unspecified return value
 
+(define silly-program
+  '(define prog
+     (lambda (l)
+       (begin
+         (write-line l)
+         (set-cdr! l 4)
+         (write-line l)))))
+
+(define fizz
+  (noisy-infer-program-types silly-program))
+#|
+(begin (define prog (lambda (l) (declare-type l (? l:2)) (write-line l) (set-cdr! l 4) (write-line l))) (declare-type prog (type:procedure ((? l:2)) (? type:9))))
+(= (? prog:1) (type:procedure ((? l:2)) (? type:9)))
+(= (? type:9) (? type:8))
+(= (? type:8) (? type:7))
+(= (? write-line:3) (type:procedure ((? l:2)) (? type:4)))
+(= (? set-cdr!:5) (type:procedure ((? l:2) (numeric-type)) (? type:6)))
+(= (? write-line:3) (type:procedure ((? l:2)) (? type:7)))
+;Value: fizz
+|#
+
+
+#|
+(pp (simplify-annotated-program fizz))
+(begin (define prog (lambda (l) (declare-type l (? l:2)) (write-line l) (set-cdr! l 4) (write-line l))) (declare-type prog (type:procedure ((? l:2)) (? type:9))))
+;Unspecified return value
+|#
+
+
+(define redef-example
+  '(define redef
+     (lambda ()
+       (begin
+
+         (define em
+           (+ 1 2))
+
+         (define +
+           (lambda (a b)
+             (cons a b)))
+         (define ajr
+           (+ 1 2))))))
+
+
+
+(define em
+  (noisy-infer-program-types redef-example))
+#|
+(begin
+ (define redef
+   (lambda ()
+     (define em
+       (+ 1 2))
+     (declare-type em (? type:34))
+     (define +
+       (lambda (a b)
+         (declare-type a (? a:36))
+         (declare-type b (? b:37))
+         (cons a b)))
+     (declare-type + (type:procedure ((? a:36) (? b:37)) (? type:40)))
+     (define ajr
+       (+ 1 2))
+     (declare-type ajr (? type:42))))
+ (declare-type redef (type:procedure () (? type:44))))
+(= (? redef:32) (type:procedure () (? type:44)))
+(= (? type:44) (? type:43))
+(= (? type:43) (? ajr:41))
+(= (? em:33) (? type:34))
+(= (type:procedure ((numeric-type) (numeric-type)) (numeric-type)) (type:procedure ((numeric-type) (numeric-type)) (? type:34)))
+(= (? +:35) (type:procedure ((? a:36) (? b:37)) (? type:40)))
+(= (? type:40) (? type:39))
+(= (? cons:38) (type:procedure ((? a:36) (? b:37)) (? type:39)))
+(= (? ajr:41) (? type:42))
+(= (? +:35) (type:procedure ((numeric-type) (numeric-type)) (? type:42)))
+;Value: em
+|#
+
+(define cdr-example
+  '(define prog
+     (lambda (l)
+       (cdr l))))
+
+(define em
+  (noisy-infer-program-types cdr-example))
+#|
+(begin (define prog (lambda (l) (declare-type l (? l:92)) (cdr l))) (declare-type prog (type:procedure ((? l:92)) (? type:96))))
+(= (? prog:91) (type:procedure ((? l:92)) (? type:96)))
+(= (? type:96) (? type:95))
+(= (type:procedure ((type:pair (? car:94) (? cdr:93))) (? cdr:93)) (type:procedure ((? l:92)) (? type:95)))
+;Value: em
+
+
+(pp (simplify-annotated-program em))
+(begin (define prog (lambda (l) (declare-type l (type:pair (? car:94) (? type:96))) (cdr l)))
+       (declare-type prog (type:procedure ((type:pair (? car:94) (? type:96))) (? type:96))))
+
+|#
+
+
 #|
 TODOs:
 Operations file: AJ
--- src/types.scm
+++ src/types.scm
@@ -1,3 +1,53 @@
 #|
 TODO: define built-in types and associated wrappers here. Expand from what exists in type-resolver.scm
 |#
+
+(define pair-type)
+(define pair-type?)
+(receive (constructor predicate)
+    (define-parametric-type-operator 'type:pair)
+  (set! pair-type constructor)
+  (set! pair-type? predicate))
+
+
+(define (get-from-env name env)
+  (cdr (let loop ((env env))
+         (or (assq name (car env))
+             (if (pair? (cdr env))
+                 (loop (cdr env))
+                 (let ((tcell (make-type-cell name)))
+                   (set-car! env (cons tcell (car env)))
+                   tcell))))))
+
+(define (get-var-type name env)
+  (let ((var-type (get-from-env name env)))
+    (if (type-expression? var-type)
+        var-type
+        (evaluate-var-type var-type))))
+
+(define (evaluate-var-type t)
+  (guarantee procedure? t)
+  (t))
+
+(define (evaluate-cdr)
+  (let ((car-type (type-variable 'car))
+        (cdr-type (type-variable 'cdr)))
+    (procedure-type (list (pair-type car-type cdr-type)) cdr-type)))
+
+(define (make-top-level-env-frame)
+  (let ((binary-numerical
+         (let ((v (numeric-type)))
+           (procedure-type (list v v) v)))
+        (binary-comparator
+         (let ((v (numeric-type)))
+           (procedure-type (list v v) (boolean-type)))))
+    (list (cons '+ binary-numerical)
+          (cons '- binary-numerical)
+          (cons '* binary-numerical)
+          (cons '/ binary-numerical)
+          (cons '= binary-comparator)
+          (cons '< binary-comparator)
+          (cons '> binary-comparator)
+          (cons '<= binary-comparator)
+          (cons '>= binary-comparator)
+          (cons 'cdr evaluate-cdr))))
