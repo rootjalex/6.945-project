0bd58b36429d217a8b628e26f73e539b664b3268
Alexander Root <aroot222@mit.edu>
2021-05-17 21:20:08 -0400

    update top level message

--- src/operations.scm
+++ src/operations.scm
@@ -1,6 +1,6 @@
 #|
-TODO: define built-in operations and associated wrappers here. Expand from what exists in type-resolver.scm
-      re-define the interface for providing built-in operation's resultant types as a register! system
+Define built-in operations and associated wrappers here. Expand from what exists in type-resolver.scm
+  Re-defines the interface for providing built-in operations' resultant types as a register! system
 |#
 
 (define top-level-env-frame '())
